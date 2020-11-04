5977552618a71106b5bad3d95efb78189acb719c
jhg3522 <jhg3522@kookmin.ac.kr>
2020-11-05 01:00:10 +0900

    splash

---
+++ app/src/main/java/com/example/clothesmygod/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.clothesmygod;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
